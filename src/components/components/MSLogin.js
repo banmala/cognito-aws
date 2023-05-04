dda1a620e4ccde1cac04b114db27703c944aef72
banmala <banmalasunil@gmail.com>
2023-05-04 16:47:12 +0545

    cognito login and authentication with google

--- src/components/components/MSLogin.js
+++ src/components/components/MSLogin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CognitoLogin = () => {
+const CognitoLoginWithMS = () => {
   const exchangeToken = async (code) => {
     const clientId = process.env.REACT_APP_Cognito_ClientId;
     const clientSecret = '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';
@@ -45,7 +45,6 @@ const CognitoLogin = () => {
     const redirectUri = 'http://localhost:3000';
     const responseType = 'code';
     const scope = 'openid email phone';
-    const state = 'AMbdmDnvGPI9JC3mCiF9Dr4_ypa8tQOeSO0OjFh6iZtCp6qpQpBwg3yxeyqCGs8c9s7yO9V1tvRCeBJCPa85O0a5Hc2K7cyHpO_ffrW2nPS25GB54eGP0qLvO3ydYdOWC-wibQXUGOO0ujfQCWVGQkcHLHWL92wPadX-o-jIa0oM1az-pHGX9a_ohaReWpI-HOZJA03XM4gd9Z5I_vfue4EwD51VmlAGcNkRL_BVGRkMjSuIGUczp2BdmCK5D71AhwXNkBGOLl4lDouvXsbn82tux8Q4WtS5XVKHjhgr0Tp2HmkU6-oz0V24S_eOxSvFrIWr4Tmkl6gfXE7OPR50lfrcfdAslJV2';
     const identityProvider = 'Google';
 
     const authorizeUrl = `https://concertodev.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}&identity_provider=${identityProvider}`;
@@ -54,8 +53,8 @@ const CognitoLogin = () => {
   };
 
   return (
-    <button onClick={handleLogin}>Login with Google</button>
+    <button onClick={handleLogin}>Login with Microsoft</button>
   );
 };
 
-export default CognitoLogin;
+export default CognitoLoginWithMS;
